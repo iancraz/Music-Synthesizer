95684e4f629de85b1284fc49aedcc349d69991de
MD-23 <MD-23@users.noreply.github.com>
2020-05-14 01:56:31 -0300

    Sampleo

---
+++ Leandro/Sample.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <cstring>
+#include "AudioFile.h" 
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+class Sample {
+public:
+	Sample(string file_path, int octave);
+	~Sample();
+
+	AudioFile<double> audioFile;
+	float* sample;
+	int* peaks;
+	int T;
+	float* h_s;
+
+	vector <float*> sample_s;
+	float pitch_frecuency;
+	int fs;
+	int lenght_peaks;
+	int lenght_sample;
+	string file_path;
+	int num_octave;
+
+	//float note_pressed_time;
+
+	void hanning(float* w, int N, short itype);
+
+
+};
+
