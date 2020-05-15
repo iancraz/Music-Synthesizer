4a12469caee0b57591fb935d0ce06a1401fde8c2
Lisandro Alvarez <lialvarez@itba.edu.ar>
2020-05-15 04:46:47 -0300

    PUSHEATE ESTA

--- Leandro/main.cpp
+++ Leandro/main.cpp
@@ -3,12 +3,19 @@
 #include <QtWidgets/QApplication>
 #include <stdio.h>
 #include <time.h>
+#include <QSplashScreen>
 
 
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
+
+	QPixmap pixmap("images/splash.png");
+	QSplashScreen splash(pixmap);
+	splash.show();
 	Leandro program;
+	program.loadData();
 	program.show();
+	splash.finish(&program);
 	return a.exec();
 }
